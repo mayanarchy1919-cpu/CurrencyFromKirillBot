ba670d8860dc11bbb16ca1d4800b95f615f682f3
mayanarchy1919-cpu <mayanarchy1919@gmail.com>
2026-03-14 16:26:28 +0400

    Adding 'help' and 'values'

--- bot.py
+++ bot.py
@@ -2,3 +2,26 @@ import telebot
 from config import config
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
+
+keys = {'Dollars': 'USD',
+        'Euro': 'EUR',
+        'Rubles': 'RUB'}
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = "Добро пожаловать! Этот бот поможет быстро конвертировать валюту по текущему курсу.\n\
+Посмотреть доступные валюты: /values \n\
+Чтобы воспользоваться введите команду в формате \n<название валюты> \
+<в какую валюту перевести> \
+<количество валюты>"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+
+bot.polling()
