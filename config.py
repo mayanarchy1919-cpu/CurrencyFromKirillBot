50bb00a01dd198cf83efc7514d96fc94d2917c58
mayanarchy1919-cpu <mayanarchy1919@gmail.com>
2026-03-15 21:40:40 +0400

    Adding README.md

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 class Config:
-    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    BOT_TOKEN = "8691374777:AAHVktrRozWtnUaT6H4q-58yLdq8TtfvPg8"
 
     currency = {'Доллар': 'USD',
                 'Евро': 'EUR',
